924226221de38b24ed971351012904528cfec0b5
Cho seongbin <csb070115@gmail.com>
2025-06-06 12:59:14 +0900

    [Gold V] Title: 보물섬, Time: 1104 ms, Memory: 119872 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -4,23 +4,18 @@ from collections import deque
 def input():
     return sys.stdin.readline().rstrip()
 
-d = [(0,1),(1,0),(0,-1),(-1,0)] # 왼 | 시계
+d = [(0,1),(1,0),(0,-1),(-1,0)]
 
 def bfs(y: int,x: int) -> int:
     li2 = [[0 for _ in range(m)] for _ in range(n)]
     dq = deque([(y,x)])
     res = 0
-
-    # print(y,x)
-
     li2[y][x] = 1
 
     while dq:
         y, x = dq.popleft()
-        # print("-----")
-        # print(y,x)
+        
         for i in range(4):
-            # print((y+d[i][0], x+d[i][1]), 0 <= y+d[i][0] < n and 0 <= x+d[i][1] < m and li[y+d[i][0]][x+d[i][1]] == "L" and li2[y+d[i][0]][x+d[i][1]] == 0)
             if 0 <= y+d[i][0] < n and 0 <= x+d[i][1] < m and li[y+d[i][0]][x+d[i][1]] == "L" and li2[y+d[i][0]][x+d[i][1]] == 0:
 
                 li2[y+d[i][0]][x+d[i][1]] = li2[y][x] + 1
@@ -29,7 +24,6 @@ def bfs(y: int,x: int) -> int:
 
     for i in range(len(li2)):
         res = max(res, *li2[i])
-        # print(*li2[i])
 
     return res - 1
 
@@ -37,14 +31,11 @@ n, m = map(int, input().split())
 
 li = [list("".join(input())) for _ in range(n)]
 
-# print(li)
-
 res = 0
 
 for i in range(n):
     for ii in range(m):
         if li[i][ii] == "L":
             res = max(res, bfs(i,ii))
-
 
 print(res)
