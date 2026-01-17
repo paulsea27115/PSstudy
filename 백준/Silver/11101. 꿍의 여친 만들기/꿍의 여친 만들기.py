86227f79c74b41b70206c3681c259bf85e067c64
Cho seongbin <csb070115@gmail.com>
2026-01-17 19:44:34 +0900

    [Silver III] Title: 꿍의 여친 만들기, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11101. 꿍의 여친 만들기/꿍의 여친 만들기.py
+++ 백준/Silver/11101. 꿍의 여친 만들기/꿍의 여친 만들기.py
@@ -1,5 +1,5 @@
 i=input
 for _ in [0]*int(i()):
-  a=i().replace(":","1=").replace(",",";");x=1e9
-  for j in i().split("|"):exec(f'{a};global x;x=min(x,max(0,int({j.replace("&","1),int(")}1)))')
-  print(x)
+	a=i().replace(":","1=").replace(",",";");x=1e9
+	for j in i().split("|"):exec(f'{a};global x;x=min(x,max(0,int({j.replace("&","1),int(")}1)))')
+	print(x)
