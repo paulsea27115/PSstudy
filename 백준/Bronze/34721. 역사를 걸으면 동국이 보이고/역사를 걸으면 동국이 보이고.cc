ae96c762be3fd7edef8bedd944af8578929b85ac
Cho seongbin <csb070115@gmail.com>
2026-01-20 11:59:52 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main() {
+    int a = 0;
+    
+    std::cin >> a;
+    
+    for(int i = 0; i < a; i++) {
+        std::cout << "I love DGU" << std::endl;
+    }
+    
+    
+    return 0;
+}
