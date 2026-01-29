e3c98acfcc17b8db5bcc701afa4dc7609818eb57
Cho seongbin <csb070115@gmail.com>
2026-01-30 00:27:41 +0900

    [Gold IV] Title: 문자열 폭발, Time: 308 ms, Memory: 6076 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <deque>
+#include <algorithm>
+
+#define ll long long int
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    std::string s, check_s;
+    
+    std::deque<char> dq;
+    
+    std::cin >> s >> check_s;
+    std::reverse(check_s.begin(), check_s.end());
+    
+    for(int i = 0; i < s.length(); i++){
+        dq.push_back(s[i]);
+        
+        if(check_s.length() <= dq.size()) {
+            std::deque<char> _dq;
+            for(int j = 0; j < check_s.length(); j++) {
+                _dq.push_back(dq.back());
+                dq.pop_back();
+            }
+            
+            if(std::string(_dq.begin(), _dq.end()) == check_s) {
+                continue;
+            } else {
+                for(int j = 0; j < check_s.length(); j++) {
+                    dq.push_back(_dq.back());
+                    _dq.pop_back();
+                }
+            }
+        }
+    }
+    
+    std::string ans(dq.begin(), dq.end());
+    
+    std::cout << (ans != "" ? ans : "FRULA");
+    return 0;
+}
