01173b370138c58636870114fff228346a4fcc0a
Cho seongbin <csb070115@gmail.com>
2026-01-31 15:09:04 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string s;
+    
+    while(true){
+        int a, b;
+        
+        std::cin >> a >> b;
+        
+        if(a == b) {
+            break;
+        }else if(a > b) {
+            s = (a % b == 0 ? "multiple" : "neither");
+        } else if(a < b) {
+            s = (b % a == 0 ? "factor" : "neither");
+        }
+        
+        std::cout << s << "\n";
+    }
+
+    return 0;
+}
