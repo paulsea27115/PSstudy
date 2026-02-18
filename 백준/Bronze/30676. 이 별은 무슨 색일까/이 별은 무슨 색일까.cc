3bb32a0e478e6719019ab3f27a5b493d360f2529
Cho seongbin <csb070115@gmail.com>
2026-02-18 21:44:42 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.cc
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.cc
@@ -5,15 +5,15 @@ using namespace std;
 
 int main()
 {
-    int li[8] = {781, 620, 590, 570, 495, 450, 425, 380};
-    string s[8] = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"}; 
+    int li[7] = {620, 590, 570, 495, 450, 425, 380};
+    string s[7] = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"}; 
 
     int n, a = 0;
     cin >> n;
     
     for(; a < 8; a++) {
         if(li[a] <= n) {
-            cout << s[a - 1];
+            cout << s[a];
             break;
         }
     }
