d631e60acdc8e6aa5537bbdf9109c39e669c6088
Cho seongbin <csb070115@gmail.com>
2026-02-21 20:00:19 +0900

    [Bronze IV] Title: Winning Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, res = 0, check = 3;
+    const int half = 3;
+    
+    for(int i = 0; i < 6; i++) {
+        cin >> a;
+        res += (i < half? a: -a) * check;
+        check -= half - 1 == i ? -2: 1;
+    }
+    
+    cout << (res == 0 ? "T" : (res < 0 ? "B" : "A"));
+
+    return 0;
+}
