12e8cb2610c08b864ba69cba2c78d4e6c17f18df
Cho seongbin <csb070115@gmail.com>
2026-03-21 21:20:58 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin >> n;
+    
+    cout << ((n)*(n-1)) / 2 << "\n" << 2;
+
+    return 0;
+}
+
+
+/*
+
+for(int i = 1; i <= n-1; i++) 
+
+    for(int j = i + 1; j <= n; j++) 
+
+
+*/
+
+
+// 1 2 
+// 2 3
+
+// 1 2 3 4 5 6
+
+// 2 3 4 5 6 7 6
+// 3 4 5 6 7 5
+// 4 5 6 7 4
+// 5 6 7 3
+// 6 7 2
+// 7 1
+
+// 3
+
+// 1 2
+
+// 2 3
+// 3
+
+
+
+
+
