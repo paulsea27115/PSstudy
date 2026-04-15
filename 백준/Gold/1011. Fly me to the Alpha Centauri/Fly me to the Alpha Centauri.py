2758bd7703891156c3d27cfd5afb3ce60733cc56
Cho seongbin <csb070115@gmail.com>
2026-04-15 21:05:17 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -1,10 +1,29 @@
 import sys
 import math
+
 n = int(sys.stdin.readline())
+
 for _ in range(n):
-    x,y=map(int,sys.stdin.readline().split())
-    n=y-x
-    sn=int(math.sqrt(n))
-    c=n-sn**2
-    print(2*sn+(-1 if c==0 else 0 if c<=sn else 1))
+    x, y = map(int,sys.stdin.readline().split())
+    
+    res = 0
+    
+    num = y - x
+    
+    sqrt_natural = math.floor(math.sqrt(num))
+    
+    natural = sqrt_natural ** 2
+    
+    check = num - natural 
+    
+    # print("----", num, sqrt_natural, check, natural)
+    
+    if check == 0:
+        res = 2 * sqrt_natural - 1
+    elif check <= sqrt_natural:
+        res = 2 * sqrt_natural
+    else:
+        res = 2 * sqrt_natural + 1
+    
+    print(res)
     
